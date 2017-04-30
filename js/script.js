52d47b587879be4a3759e42e7d366b45dd6e2730
jrbrashear <j_brashear@hotmail.com>
2017-04-30 17:43:33 -0400

    random print function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 //create quotes array with specified objects
 var quotes = [
   {quote: "A creative man is motivated by the desire to achieve, not by the desire to beat others.",
-  source: "Ayn Rand", citation: "", year: ""},
+  source: "Ayn Rand"},
   {quote: "Don't go around saying the World owes you a livining.  The World owes you nothing.  It was here first.",
-  source: "Mark Twain", citation: "", year: ""},
+  source: "Mark Twain"},
   {quote: "By trying we can easily learn to endure adversity.  Another man's, I mean.",
   source: "Mark Twain", citation: "Mark Twain Calendar", year: "1917" },
   {quote: "In Paris they just simply opened their eyes and stared when we spoke to them in French! We never did succeed in making those idiots understand their own language.",
-  source: "Mark Twain", citation: "The Innocents Abroad", year:"" }
+  source: "Mark Twain", citation: "The Innocents Abroad", }
 ]
-console.log (quotes[3].quote);
+
+function getRandomQuote()
+ {
+   var randomQuote = quotes[Math.floor(Math.random()* quotes.length)];
+   return randomQuote;
+ };
+
+ function printQuote() {
+   var printQuote = getRandomQuote();
+   printQuote += '<p class="quote">' + printQuote.quote + ' </p>;'
+   return printQuote;
+
+ };
+
+ 
+
+
+console.log(printQuote());
